73f38836b4e7cc3147c800cf4b805a1c5d4494db
kgreed1 <kgreed1@sheffield.ac.uk>
2022-01-28 17:01:21 +0000

    preprocessing scripts

--- Preprocessing/preprocess.sh
+++ Preprocessing/preprocess.sh
@@ -1,8 +1,19 @@
-#tokenize
-python Preprocessing/tokenizer.py
+#!/bin/bash 
+#SBATCH --nodes=1 
+#SBATCH --mem=32G 
+#SBATCH --mail-user=kgreed1@sheffield.ac.uk 
+#SBATCH --comment=preprocess
+#SBATCH --output=output.%j.preprocess.out
 
-echo "Tokenize done"
+#tokenize
+#python Preprocessing/tokenizer.py
+
+#echo "Tokenize done"
 
 # bpe
-subword-nmt learn-bpe -s 32000 < DialogSum_Data/dialogsum.sample(150).tok > codes_file.bpe
-subword-nmt apply-bpe -c codes_file.bpe < DialogSum_Data/dialogsum.sample(150).tok.bpe
+
+module load python/anaconda3
+source activate dialogsum
+
+subword-nmt learn-bpe -s 32000 < DialogSum_Data/dialogsum.sample.jsonl > codes_file.bpe
+subword-nmt apply-bpe -c codes_file.bpe < DialogSum_Data/dialogsum.sample.tok.bpe
--- Preprocessing/tokenizer.py
+++ Preprocessing/tokenizer.py
@@ -1,11 +1,13 @@
+import nltk
 from nltk import word_tokenize
+import os
 
 nltk.download('punkt')
 
 if __name__=="__main__":
     # tok
-    with open('DialogSum_Data/dialogsum.sample(150).jsonl', "r") as f1:
-        with open('DialogSum_Data/dialogsum.sample(150).tok', "w") as f2:
+    with open('DialogSum_Data/dialogsum.sample.jsonl', "r") as f1:
+        with open('DialogSum_Data/dialogsum.sample.tok', "w+") as f2:
             for line in f1:
                 words = word_tokenize(line)
                 f2.write(' '.join(words) + '\n')   
