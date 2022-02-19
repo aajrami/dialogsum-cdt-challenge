c9ac2c9add925991b0dfb3526ffde642adbf15c2
acidrobin <jclayton558@gmail.com>
2022-02-19 14:18:31 +0000

    test commit

--- run_model.sh
+++ run_model.sh
@@ -1,7 +1,3 @@
 #!/bin/bash 
 
-<<<<<<< HEAD
-python scripts/train.py --EXPERIMENT_NAME "preliminary_test" --N_EPOCHS 300
-=======
 python scripts/train.py --EXPERIMENT_NAME "preliminary_test" --N_EPOCHS 20
->>>>>>> 6eaa3df240e639735a84f28d9f3ba461c5fba37d
